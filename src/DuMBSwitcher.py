d1716e6f464acd1ee526c2bc72a43e372e6de055
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2018-08-12 16:22:30 -0600

    Initial Commit

---
+++ src/DuMBSwitcher.py
@@ -0,0 +1,13 @@
+
+
+# TODO: Get command line args
+
+# TODO: Create blank image the size of monitor space
+
+# TODO: Place images in blank image at correct position and scale
+
+# TODO: Set as wallpaper
+
+# TODO: Allow slideshow
+
+
